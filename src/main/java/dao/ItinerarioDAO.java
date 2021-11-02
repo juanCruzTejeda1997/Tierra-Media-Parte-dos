4c5f5b7db57ff52839d2be1c22095145fa26892c
MelisaALopez <87398751+MelisaALopez@users.noreply.github.com>
2021-11-01 23:33:55 -0300

    Add files via upload

--- src/main/java/dao/ItinerarioDAO.java
+++ src/main/java/dao/ItinerarioDAO.java
@@ -1,10 +1,13 @@
 package dao;
 
+import java.util.LinkedList;
+
 import model.Itinerario;
 
 public interface ItinerarioDAO extends GenericDAO<Itinerario> {
 	
 		public abstract Itinerario buscarItinerarioPorNombre(String nombre);
+		public LinkedList<Itinerario> buscarItinerarioPorUsuario(String nombre);
 		
 	
 
